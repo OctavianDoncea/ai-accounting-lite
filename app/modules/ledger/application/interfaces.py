d38ce4edf88b042852ba2a7202924bba58c19a33
Octavian Doncea <octaviandoncea@gmail.com>
2026-02-26 15:31:45 +0200

    Completed unit of Work file

--- app/modules/ledger/application/interfaces.py
+++ app/modules/ledger/application/interfaces.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-from ast import Pass
+from app.core.database import AsyncSessionLocal
+from app.modules.ledger.infrastructure.repositories import AccountRepository, JournalEntryRepository
+from app.modules.receipts.infrastructure.repositories import ReceiptRepository
 
 class UnitOfWork(ABC):
     @abstractmethod
@@ -13,10 +15,46 @@ class UnitOfWork(ABC):
 
     @property
     @abstractmethod
-    def accounts(self): pass
+    def accounts(self) -> AccountRepository: pass
     @property
     @abstractmethod
-    def journal_entries(self): pass
+    def journal_entries(self) -> JournalEntryRepository: pass
     @property
     @abstractmethod
-    def receipts(self): pass
+    def receipts(self) -> ReceiptRepository: pass
+
+class SQLALchemyUnitOfWork(UnitOfWork):
+    def __init__(self):
+        self.session_factory = AsyncSessionLocal
+        self.session = None
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+        self.accounts = AccountRepository(self.session)
+        self._journal_entries = JournalEntryRepository(self.session)
+        self._receipts = ReceiptRepository(self.session)
+
+        return self
+
+    async def _aexit_(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            await self.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
+
+    @property
+    def accounts(self) -> AccountRepository:
+        return self._accounts
+
+    @property
+    def journal_entries(self) -> JournalEntryRepository:
+        return self._journal_entries
+
+    @property
+    def receipts(self) -> ReceiptRepository:
+        return self._receipts
