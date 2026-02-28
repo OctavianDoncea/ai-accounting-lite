7d236b79274f3ca02dbb431bb782aaeb324d756f
Octavian Doncea <octaviandoncea@gmail.com>
2026-02-28 20:59:29 +0200

    Generated new migration

--- alembic/env.py
+++ alembic/env.py
@@ -12,8 +12,6 @@ from app.modules.receipts.infrastructure.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-fileConfig(config.config_file_name)
-target_metadata = Base.metadata
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -24,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
